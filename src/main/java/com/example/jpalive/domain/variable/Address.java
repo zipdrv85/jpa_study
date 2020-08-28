135afd3bde0144939c55e58e5fcab1a153378fdc
inzentUser <zipdrv85@gmail.com>
2020-08-28 13:46:28 +0900

    example source 제거

--- src/main/java/com/example/jpalive/domain/variable/Address.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpalive.domain.variable;
-
-public class Address {
-	private String city;
-	private String street;
-	private String zipcode;
-}
--- src/main/java/com/example/jpalive/domain/variable/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.jpalive.domain.variable;
-
-public enum DeliveryStatus {
-	READY, COMPLETE
-}
